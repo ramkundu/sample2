54b311a47f01e781f7a4bea537d6dffac290de27
ramkundu <ramkrishnakunduece@gmail.com>
2014-11-20 19:41:52 +0530

    app2

---
+++ demogit2/demogit2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demogit2
+//
+//  Created by Ramakrishna on 20/11/14.
+//  Copyright (c) 2014 Ramakrishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
